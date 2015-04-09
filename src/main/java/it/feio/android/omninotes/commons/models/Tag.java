5ee0c00d215377fd3c1917e267d1d6623f5e6a1b
Federico Iosue <federico.iosue@gmail.com>
2015-04-10 01:59:23 +0200

    Tag changes

--- src/main/java/it/feio/android/omninotes/commons/models/Tag.java
+++ src/main/java/it/feio/android/omninotes/commons/models/Tag.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 Federico Iosue (federico.iosue@gmail.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,56 +17,38 @@ package it.feio.android.omninotes.commons.models;
 
 
 public class Tag {
-	private Integer id;
-	private String name;
-	private String description;
-	private String color;
 
-	public Tag(){
-		super();
-	}
-	
-	
-	public Tag(Integer id, String title, String description, String color) {
-		super();
-		this.id = id;
-		this.name = title;
-		this.description = description;
-		this.color = color;
-	}
+    private String text;
+    private int count;
 
 
+    public Tag() {
+        super();
+    }
 
 
-	public Integer getId() {
-		return id;
-	}
+    public Tag(String text, int count) {
+        this.text = text;
+        this.count = count;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
-	public String getName() {
-		return name;
-	}
+    public int getCount() {
+        return count;
+    }
 
-	public void setName(String title) {
-		this.name = title;
-	}
 
-	public String getDescription() {
-		return description;
-	}
+    public void setCount(int count) {
+        this.count = count;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
-	public String getColor() {
-		return color;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setColor(String color) {
-		this.color = color;
-	}
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
