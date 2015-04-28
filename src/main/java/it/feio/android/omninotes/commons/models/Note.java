4e13e95e8d38c9ef778b1064c9edbd06e7316aff
Federico Iosue <fiosue@omniagroup.it>
2015-04-29 01:14:27 +0200

    Added a flag to keep track of fired reminders

--- src/main/java/it/feio/android/omninotes/commons/models/Note.java
+++ src/main/java/it/feio/android/omninotes/commons/models/Note.java
@@ -28,6 +28,7 @@ public class Note implements Serializable {
 	private Boolean archived;
 	private Boolean trashed;
 	private String alarm;
+	private Boolean reminderFired;
 	private String recurrenceRule;
 	private Double latitude;
 	private Double longitude;
@@ -51,7 +52,7 @@ public class Note implements Serializable {
 
 
 	public Note(int _id, Long creation, Long lastModification, String title, String content, Integer archived,
-			Integer trashed, String alarm, String recurrenceRule, String latitude, String longitude, Category 
+			Integer trashed, String alarm, Integer reminderFired, String recurrenceRule, String latitude, String longitude, Category
 			category, Integer locked, Integer checklist) {
 		super();
 		this._id = _id;
@@ -59,15 +60,16 @@ public class Note implements Serializable {
 		this.content = content;
 		this.creation = creation;
 		this.lastModification = lastModification;
-		this.archived = archived == 1 ? true : false;
-		this.trashed = trashed == 1 ? true : false;
+		this.archived = archived == 1;
+		this.trashed = trashed == 1;
 		this.alarm = alarm;
+		this.reminderFired = reminderFired == 1;
 		this.recurrenceRule = recurrenceRule;
 		setLatitude(latitude);
 		setLongitude(longitude);
 		setAddress(address);
-		setLocked(locked == 1 ? true : false);
-		setChecklist(checklist == 1 ? true : false);
+		setLocked(locked == 1);
+		setChecklist(checklist == 1);
 	}
 
 
@@ -82,6 +84,7 @@ public class Note implements Serializable {
 		setTrashed(note.isTrashed());
 		setAlarm(note.getAlarm());
 		setRecurrenceRule(note.getRecurrenceRule());
+		setReminderFired(note.isReminderFired());
 		setLatitude(note.getLatitude());
 		setLongitude(note.getLongitude());
 		setAddress(note.getAddress());
@@ -171,7 +174,7 @@ public class Note implements Serializable {
 
 
 	public Boolean isArchived() {
-		return archived == null || archived == false ? false : true;
+		return !(archived == null || !archived);
 	}
 
 
@@ -181,12 +184,12 @@ public class Note implements Serializable {
 
 
 	public void setArchived(int archived) {
-		this.archived = archived == 1 ? true : false;
+		this.archived = archived == 1;
 	}
 
 
 	public Boolean isTrashed() {
-		return trashed == null || trashed == false ? false : true;
+		return !(trashed == null || !trashed);
 	}
 
 
@@ -196,7 +199,7 @@ public class Note implements Serializable {
 
 
 	public void setTrashed(int trashed) {
-		this.trashed = trashed == 1 ? true : false;
+		this.trashed = trashed == 1;
 	}
 
 
@@ -212,6 +215,21 @@ public class Note implements Serializable {
 
 	public void setAlarm(long alarm) {
 		this.alarm = String.valueOf(alarm);
+	}
+
+
+	public Boolean isReminderFired() {
+		return !(reminderFired == null || !reminderFired);
+	}
+
+
+	public void setReminderFired(Boolean reminderFired) {
+		this.reminderFired = reminderFired;
+	}
+
+
+	public void setReminderFired(int reminderFired) {
+		this.reminderFired = reminderFired == 1;
 	}
 
 
@@ -278,7 +296,7 @@ public class Note implements Serializable {
 
 
 	public Boolean isLocked() {
-		return locked == null || locked == false ? false : true;
+		return !(locked == null || !locked);
 	}
 
 
@@ -288,12 +306,12 @@ public class Note implements Serializable {
 
 
 	public void setLocked(int locked) {
-		this.locked = locked == 1 ? true : false;
+		this.locked = locked == 1;
 	}
 
 
 	public Boolean isChecklist() {
-		return checklist == null || checklist == false ? false : true;
+		return !(checklist == null || !checklist);
 	}
 
 
@@ -303,7 +321,7 @@ public class Note implements Serializable {
 
 
 	public void setChecklist(int checklist) {
-		this.checklist = checklist == 1 ? true : false;
+		this.checklist = checklist == 1;
 	}
 
 
@@ -371,9 +389,7 @@ public class Note implements Serializable {
 
 
 	public boolean isChanged(Note note) {
-		boolean res = false;
-		res = !equals(note) || !getAttachmentsList().equals(note.getAttachmentsList());
-		return res;
+		return !equals(note) || !getAttachmentsList().equals(note.getAttachmentsList());
 	}
 
 
@@ -383,9 +399,7 @@ public class Note implements Serializable {
 		emptyNote.setCreation(getCreation());
 		emptyNote.setCategory(getCategory());
 		// Check
-		if (isChanged(emptyNote))
-			return false;
-		else return true;
+		return !isChanged(emptyNote);
 	}
 
 
